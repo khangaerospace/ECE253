9e1bbeb9e69b8e75bf1c741e9265fec30a3f394d
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2025-11-03 22:09:28 -0500

    lab6

---
+++ lab6/part1.s
@@ -0,0 +1,31 @@
+.global _start
+.text
+
+_start:
+	la s2, LIST
+	addi s10, zero, 0
+	addi s11, zero, 0
+
+LOOP:
+	# find the list
+	lw s3, 0(s2) # load current number
+	addi s4, zero, -1
+	beq s3, s4, END # end if we get to -1
+	
+	add s10, s10, s3 # add the sum
+	add s11, s11, 1 # add 1
+	
+	addi s2,s2, 4 # move to the next value
+	j LOOP # continue the loop
+	
+	
+	
+
+END:
+	j END
+
+.global LIST
+.data
+LIST:
+.word 1, 2, 3, 5, 0xA, -1
+	
